6cca6df32f8a4a48f1d87b886816e426cdee6d20
Noor Tauqir <aditauqir@gmail.com>
2026-05-15 11:30:51 -0400

    Initial Commit

---
+++ Zehan/ZehanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZehanApp.swift
+//  Zehan
+//
+//  Created by Adi Tauqir on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ZehanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
